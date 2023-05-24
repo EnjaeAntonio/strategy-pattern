7e38a4e0e365a3982802ae77fb9ee0f328129aea
Enjae Antonio <enjaeantonio@outlook.com>
2023-05-24 17:38:18 -0500

    abstract classes created

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿public abstract class Context
+{
+    
+}
+
+public abstract class ReceiveRadio : Context
+{
+    protected ReceiveRadio() { }
+}
+
+public abstract class Hardware : Context
+{
+
+}
+
+public abstract class Receivers : Hardware
+{
+
+}
+public abstract class Amplifiers : Hardware
+{
+
+}
+
+public abstract class Radios : Hardware
+{
+
+}
