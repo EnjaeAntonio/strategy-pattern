542d4686901fd0a2de62877c31b81c8797b868c9
Enjae Antonio <enjaeantonio@outlook.com>
2023-05-28 12:07:13 -0500

    decorators need methods

--- Program.cs
+++ Program.cs
@@ -1,66 +1,97 @@
 ﻿public abstract class Client
 {
-    public abstract void GetReputation();
+    public abstract int GetReputation();
     public abstract void GetPriveleges();
 }
 
 public class User : Client
 {
-
-    private int _reputation { get; set; } 
+    private int _reputation = 0;
     public override void GetPriveleges()
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void GetReputation()
     {
         void _grantBasicAccess()
         {
             Console.WriteLine("User now has basic access wooh!");
         }
     }
+
+    public User(int reputation)
+    {
+        _reputation = reputation;
+    }
+
+    public override int GetReputation()
+    {
+        return _reputation;
+    }
+
 }
 
-public abstract class BadeDecorator : Client 
+public abstract class BadgeDecorator : Client 
 {
     protected Client _client;
-    public abstract override void GetPriveleges();
-    public abstract override void GetReputation();
-    
+    public override void GetPriveleges()
+    {
+        _client.GetPriveleges();
+    }
+    public override int GetReputation()
+    {
+        return _client.GetReputation();
+    }
+
+    public BadgeDecorator(Client client)
+    {
+        _client = client;
+    }
 }
-public class CommunityBadge : BadeDecorator
+public class CommunityBadge : BadgeDecorator
 {
+    public CommunityBadge(Client client) : base(client)
+    {
+    }
+
+    public override void GetPriveleges()
+    {
+        void _grantBasicAccess()
+        {
+
+        }
+    }
+
+
+    public override int GetReputation()
+    {
+        return _client.GetReputation() + 5;
+    }
+}
+public class BannedBadge : BadgeDecorator
+{
+    public BannedBadge(Client client) : base(client)
+    {
+    }
+
     public override void GetPriveleges()
     {
         throw new NotImplementedException();
     }
 
-    public override void GetReputation()
+    public override int GetReputation()
     {
         throw new NotImplementedException();
     }
 }
-public class BannedBadge : BadeDecorator
+public class HundredPostsBadge : BadgeDecorator
 {
+    public HundredPostsBadge(Client client) : base(client)
+    {
+    }
+
     public override void GetPriveleges()
     {
         throw new NotImplementedException();
     }
 
-    public override void GetReputation()
-    {
-        throw new NotImplementedException();
-    }
-}
-public class HundredPostsBadge : BadeDecorator
-{
-    public override void GetPriveleges()
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void GetReputation()
+    public override int GetReputation()
     {
         throw new NotImplementedException();
     }
